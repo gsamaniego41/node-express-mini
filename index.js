0f36b64bdcefd36171e28b6c9f4ed7062c3da6ff
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 15:23:34 -0700

    get all users

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const PORT = 4000;
 
 server.use(express.json());
 
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({message: "Something went wrong"}));
+});
+
 server.listen(PORT, console.log(`Port ${PORT} is listening`));
