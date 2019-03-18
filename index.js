61cdc701ecb8e0e16c257bb19b19080de8c1b480
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 15:17:50 -0700

    boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// implement your API here
+const express = require("express");
+const server = express();
+const db = require("./data/db");
+const PORT = 4000;
+
+server.use(express.json());
+
+server.listen(PORT, console.log(`Port ${PORT} is listening`));
