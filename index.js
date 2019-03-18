f44430563e4aece43888b584bc3cd9ee2de0afd6
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-03-18 15:27:28 -0700

    POST - adds a new user

--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const PORT = 4000;
 
 server.use(express.json());
 
+const message = "Oops something went wrong";
+
 server.get("/api/users", (req, res) => {
   db.find()
     .then(users => res.status(200).json(users))
-    .catch(err => res.status(500).json({message: "Something went wrong"}));
+    .catch(err => res.status(500).json({message}));
+});
+
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  db.insert(newUser)
+    .then(user => res.status(201).json(user))
+    .catch(err => res.status(500).json({message}));
 });
 
 server.listen(PORT, console.log(`Port ${PORT} is listening`));
